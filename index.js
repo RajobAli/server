fc1ab82b9262590e221229811c17b8bf1d8ad01e
MD Rajob Ali <119127566+RajobAli@users.noreply.github.com>
2023-02-18 21:54:58 +0600

    server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/register",(req,res)=>{
+    const fullName = req.body.fullName;
+    const age = req.body.age;
+  res.send(`<h2>Your name is ${fullName} <br> Your age is ${age} </h2>`);
+});
+app.listen(PORT,()=>{
+    console.log(`server is running  at http://localhost:${PORT}`);
+});
